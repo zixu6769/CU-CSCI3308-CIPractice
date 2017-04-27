4bb27f3e7557795c1ec962afdb89c0951dabe0d9
zixu6769 <zixu6769@colorado.edu>
2017-04-27 17:22:23 -0600

    added travis

--- geometry.c
+++ geometry.c
@@ -48,6 +48,10 @@ bool coord_2d_eq(const coord_2d_t* a, const coord_2d_t* b){
 
 }
 
+double coord_2d_area_triangle(const coord_2d_t *a, const coord_2d_t *b, const coord_2d_t *c) {
+	double triArea;
+	return triArea = abs((a->x *(b->y - c->y) + b->x * (c->y - a->y) + c->x * (a->y - b->y))/2);
+}
 void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b){
 
     /* Input Checks */
@@ -69,3 +73,4 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+double coord_2d_area_triangle(const coord_2d_t *a, const coord_2d_t *b, const coord_2d_t *c);
--- geometry_test.c
+++ geometry_test.c
@@ -14,7 +14,21 @@
 #include <check.h>
 
 #include "geometry.h"
+START_TEST(test_2d_area_triangle) {
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
 
+	a.x=b.x=c.x=a.y=b.y=c.y=0;
+	ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0);
+	
+	a.x=a.y=0;
+	b.x=b.y=200;
+	c.x=0;
+	c.y=200;
+	ck_assert(coord_2d_area_triangle(&a, &b, &c) == 20000);
+}
+END_TEST
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
 {
@@ -162,11 +176,13 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_2d_area_triangle);
     /* Return Suite */
     return s;
 
